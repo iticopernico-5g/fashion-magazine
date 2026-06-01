e2181b061bd9894a272aa84c64494b80d932eeb9
flores-cr <crisreyven.flores@iticopernico.it>
2026-06-02 00:21:11 +0200

    Fix: ripristinati underscore nei valori enum Category, aggiornato DB

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,10 @@ namespace App\Models;
 enum Category : string {
     case Sport = 'sport';
     case Fashion = 'fashion';
-    case SocialActivity = 'social activity';
+    case SocialActivity = 'social_activity';
     case Charity = 'charity';
     case Challenges = 'challenges';
-    case StyleRecommendations = 'style recommendations';
+    case StyleRecommendations = 'style_recommendations';
     case Events = 'events';
     case Other = 'other';
 }
