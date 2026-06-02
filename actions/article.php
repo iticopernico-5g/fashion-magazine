33ef270ee700546d18eff990d41c2b0423ec75f8
flores-cr <crisreyven.flores@iticopernico.it>
2026-06-02 20:59:37 +0200

    Fix delete articolo e restyle card a griglia compatta con crop immagini

--- actions/article.php
+++ actions/article.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../camezilla/camezilla.php';
 use App\Models\Article;
 use App\Models\Category;
 use App\Models\Status;
-use DateTime;
 use App\Services\ArticleService;
 use Camezilla\Dispatchers\Dispatcher;
 
@@ -73,7 +72,7 @@ $dispatcher->post('update', function($params) use ($articleService) {
 
 $dispatcher->post('delete', function($params) use ($articleService) {
     require_user_authentication();
-    $article = new Article($params['id'], null, null, null, null, null, null, null, null, null);
+    $article = new Article($params['id'], null, null, null, null, null, null, null, null, null, null);
 
     try {
         $articleService->delete($article);
